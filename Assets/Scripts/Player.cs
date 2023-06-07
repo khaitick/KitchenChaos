24c6a8d341dfe1ae858940f23075ab15b680f4fb
Voon Khai Tick <voonkhaitick@gmail.com>
2023-06-07 21:14:32 +0800

    Update player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float time;
-    private int counter = 0;
+
+    [SerializeField] private float moveSpeed;
+
+    private Vector2 inputVector;
+    private float speed = 0;
+
+    private void Start()
+    {
+        speed = moveSpeed;
+    }
 
     private void Update()
     {
-        time += Time.deltaTime;
-        if(time > counter)
+        inputVector = new Vector2(0, 0);
+        speed = moveSpeed;
+
+        if (Input.GetKey(KeyCode.W))
         {
-            counter += 1;
-            Debug.Log(counter);
+            inputVector.y += 1;
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y += -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x += -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position +=  moveDir * Time.deltaTime * speed;
+        Debug.Log(inputVector);
     }
 }
