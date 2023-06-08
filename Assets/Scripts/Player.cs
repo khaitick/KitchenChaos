c8e6730ef70eb3cff4a589d0412f444606a5b62a
Voon Khai Tick <voonkhaitick@gmail.com>
2023-06-09 01:05:47 +0800

    Update Player.cs
    - Add Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 25f;
 
+    private float playerRadius = 0.7f;
+    private float playerHeight = 2f;
     private bool isWalking;
+
     private void Update()
     {
         Walk(gameInput.GetMovementVector());
@@ -14,11 +17,41 @@ public class Player : MonoBehaviour
     private void Walk(Vector2 inputVector)
     {
         isWalking = inputVector != Vector2.zero;
+
         if (isWalking)
         {
             Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
-            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+            float moveDistance = moveSpeed * Time.deltaTime;
+            bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+            if (!canMove)
+            {
+                // Attempt only X movement
+                Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirX;
+                }
+                else
+                {
+                    // Attempt only Z movement
+                    Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                    canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                    if (canMove)
+                    {
+                        moveDir = moveDirZ;
+                    }
+                }
+            }
+
+            if (canMove)
+            {
+                transform.position += moveDir * moveDistance;
+                transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+            }
         }
     }
 
